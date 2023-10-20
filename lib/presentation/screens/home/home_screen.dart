1347e4fc2c5cc6d06574a30d6f6056d7acf99de8
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-19 22:03:17 -0300

    :sparkles: (theme) added theme and style app

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScree extends StatelessWidget {
+  const HomeScree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter + Material 3',
+        ),
+      ),
+    );
+  }
+}
