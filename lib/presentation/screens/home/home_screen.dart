38d368a36c92e045ce2225da38464e9404975077
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-19 23:16:23 -0300

    :sparkles: (home-screen) implemented home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScree extends StatelessWidget {
   const HomeScree({super.key});
@@ -11,6 +12,47 @@ class HomeScree extends StatelessWidget {
           'Flutter + Material 3',
         ),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    appMenuItems;
+
+    return ListView.builder(
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItems[index];
+          return _CustomListTitle(menuItem: menuItem);
+        });
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(
+        menuItem.icon,
+        color: colors.primary,
+      ),
+      trailing: Icon(Icons.arrow_forward_ios_rounded),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {},
     );
   }
 }
