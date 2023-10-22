51a52e95f3e326dd4cbb8f335d347e287d56cbec
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-22 02:50:31 -0300

    :sparkles: (snackbar) snackbars dialogs finished

--- lib/presentation/screens/snackbar/snacbar_screen.dart
+++ lib/presentation/screens/snackbar/snacbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -20,6 +21,26 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estás Seguro?'),
+        content: const Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis ante suscipit orci ornare bibendum. Nunc vitae luctus sem. Donec in laoreet mi, vel aliquam nibh. Morbi libero ex, interdum vel viverra non, pretium a ante. Fusce et tortor non ipsum aliquam rhoncus. Curabitur at turpis venenatis, fringilla metus ac, pharetra neque. Nam aliquam, justo in accumsan dictum, lectus neque dignissim lorem, vel dapibus magna purus eu risus. Ut varius leo a turpis rutrum, et fermentum tortor mollis. Vestibulum in magna non elit lobortis scelerisque. Maecenas fringilla efficitur erat, a convallis nisl feugiat at.'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text('Cancelar'),
+          ),
+          FilledButton(
+              onPressed: () => context.pop(), child: const Text('Aceptar'))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +50,24 @@ class SnackbarScreen extends StatelessWidget {
         label: const Text('Mostrar Snackbar'),
         icon: const Icon(Icons.remove_red_eye_outlined),
       ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(context: context, children: [
+                  const Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis ante suscipit orci ornare bibendum. Nunc vitae luctus sem. Donec in laoreet mi, vel aliquam nibh. Morbi libero ex, interdum vel viverra non, pretium a ante. Fusce et tortor non ipsum aliquam rhoncus. Curabitur at turpis venenatis, fringilla metus ac, pharetra neque. Nam aliquam, justo in accumsan dictum, lectus neque dignissim lorem, vel dapibus magna purus eu risus. Ut varius leo a turpis rutrum, et fermentum tortor mollis. Vestibulum in magna non elit lobortis scelerisque. Maecenas fringilla efficitur erat, a convallis nisl feugiat at.')
+                ]);
+              },
+              child: const Text('Licencias usadas')),
+          FilledButton.tonal(
+            onPressed: () => openDialog(context),
+            child: const Text('Mostrar díalogo'),
+          ),
+        ],
+      )),
     );
   }
 }
