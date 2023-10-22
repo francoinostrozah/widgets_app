5fbe23c0e1bdc17bb23ea6cf0ea4647f41887ec3
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-22 13:41:31 -0300

    :sparkles: (infinite-scroll) added infinite scroll view

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -49,4 +49,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Pequeño tutorial introductorio',
       link: '/tutorial',
       icon: Icons.accessible_rounded),
+  MenuItem(
+      title: 'InfineteScroll y Pull',
+      subTitle: 'Listas infinitas y pull to refresh',
+      link: '/infinite',
+      icon: Icons.list_alt_rounded),
 ];
