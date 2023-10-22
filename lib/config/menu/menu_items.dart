f4fa541121b49d467b95be34d61ff7229e229d97
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-22 11:35:47 -0300

    :sparkles: (tiles) added SwithListTile and RadioListTile

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -39,4 +39,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Stateful widget animado',
       link: '/animated',
       icon: Icons.check_box_outline_blank_outlined),
+  MenuItem(
+      title: 'UI Controls + Tiles',
+      subTitle: 'Una serie de controles de Fluter',
+      link: '/ui-controls',
+      icon: Icons.car_rental_outlined),
 ];
--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -7,6 +7,44 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('UI Controls')),
+      body: const _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation { car, plane, boat, sumarine }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(physics: const ClampingScrollPhysics(), children: [
+      SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: isDeveloper,
+          onChanged: (value) => setState(() {
+                isDeveloper = !isDeveloper;
+              })),
+      ...Transportation.values.map((transportation) => RadioListTile(
+          title: Text('By ${transportation.name}'),
+          subtitle: Text('Travel by ${transportation.name}'),
+          value: transportation,
+          groupValue: selectedTransportation,
+          onChanged: (value) => setState(() {
+                selectedTransportation = transportation;
+              }))),
+    ]);
   }
 }
